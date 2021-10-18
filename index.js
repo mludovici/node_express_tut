016eb09b9574af8d04a434ca79a42cc0b0663ccf
Marc <ludovicimarc@gmail.com>
2021-10-18 19:07:50 +0200

    test

---
+++ index.js
@@ -0,0 +1,67 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var redis = require('redis');
+var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var cookieParser = require('cookie-parser');
+var expressSession = require("express-session");
+var redisStore = require('connect-redis')(expressSession);
+var redisClient = redis.createClient();
+app.use(expressSession({
+    // resave: true,
+    // saveUninitialized: true,
+    secret: 'woho'
+}));
+//SET VARIABLES with .set
+app.set('PORT', process.env.PORT || 3333);
+//COOKIE HANDLING
+app.use(cookieParser());
+//SERVE STATIC DIRECTORY
+app.use('/', express.static('public'));
+//PARSE BODIES FROM URL with extended true for any type, e.g multipart form data
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// app.get('/', (request, response) => {
+// 	response.send('Hello from express.js')
+// })
+app.get('/api/sayHello/:name', function (req, res, next) {
+    var name = req.params.name;
+    if (!isNaN(name)) {
+        res.status(400).send('No string as name');
+    }
+    else {
+        res.json({
+            message: "Hallo " + name
+        });
+    }
+});
+app.get('/session', function (req, res) {
+    //req.session.name = req.session.name || new Date().toUTCString()
+    console.log(req.sessionID);
+    res.json(req.session);
+});
+app.post('/api/sayHello/', upload.array(), function (req, res, next) {
+    var name = req.body.name;
+    if (!isNaN(name)) {
+        res.status(400).send('No string as name');
+    }
+    else {
+        res.json({
+            message: "Hallo " + name
+        });
+    }
+});
+app.get('/cookie', function (req, res) {
+    if (req.cookies.visited) {
+        res.send('Du hast die Cookies schon genutzt.');
+    }
+    else {
+        res.cookie('visited', true);
+        res.send('DU besuchst die cookies zum ersten mal.');
+    }
+});
+app.listen(3333, function () { return console.log("app listen on port " + process.env.PORT); });
---
+++ index.ts
@@ -0,0 +1,78 @@
+import express = require('express')
+const redis = require('redis')
+const app = express()
+let bodyParser = require('body-parser')
+let multer = require('multer')
+let upload = multer()
+let cookieParser = require('cookie-parser')
+import expressSession = require('express-session')
+
+let redisStore = require('connect-redis')(expressSession)
+let redisClient = redis.createClient()
+
+app.use(
+	expressSession({
+		// resave: true,
+		// saveUninitialized: true,
+		secret: 'woho',
+		//store: new redisStore({ client: redisClient }),
+	})
+)
+
+//SET VARIABLES with .set
+app.set('PORT', process.env.PORT || 3333)
+
+//COOKIE HANDLING
+app.use(cookieParser())
+
+//SERVE STATIC DIRECTORY
+app.use('/', express.static('public'))
+
+//PARSE BODIES FROM URL with extended true for any type, e.g multipart form data
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+)
+// app.get('/', (request, response) => {
+// 	response.send('Hello from express.js')
+// })
+
+app.get('/api/sayHello/:name', (req, res, next) => {
+	let name = req.params.name
+	if (!isNaN(name)) {
+		res.status(400).send('No string as name')
+	} else {
+		res.json({
+			message: `Hallo ${name}`,
+		})
+	}
+})
+
+app.get('/session', (req, res) => {
+	//req.session.name = req.session.name || new Date().toUTCString()
+	console.log(req.sessionID)
+	res.json(req.session)
+})
+
+app.post('/api/sayHello/', upload.array(), (req, res, next) => {
+	let name = req.body.name
+	if (!isNaN(name)) {
+		res.status(400).send('No string as name')
+	} else {
+		res.json({
+			message: `Hallo ${name}`,
+		})
+	}
+})
+
+app.get('/cookie', function (req, res) {
+	if (req.cookies.visited) {
+		res.send('Du hast die Cookies schon genutzt.')
+	} else {
+		res.cookie('visited', true)
+		res.send('DU besuchst die cookies zum ersten mal.')
+	}
+})
+
+app.listen(3333, () => console.log(`app listen on port ${process.env.PORT}`))
